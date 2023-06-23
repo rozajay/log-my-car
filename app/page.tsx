d3abf05bce37a9874931fdd395fdcebf6df73e65
Roza <junzhujiang@gmail.com>
2023-06-23 16:37:52 +1000

    feat: add in dropdown logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,32 @@
-"use client"
+'use client'
 import React, { ChangeEvent, useState } from 'react';
 import Dropdown from './UI/Dropdown';
 
+interface FormData {
+  make: string;
+  model: string;
+  badge: string;
+  file: File | null;
+}
 
 const Home: React.FC = () => {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = useState<FormData>({
     make: '',
     model: '',
     badge: '',
-    file: null
+    file: null,
   });
 
   const handleChange = (event: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
     if (event.target.type === 'file') {
       setFormData({
         ...formData,
-        [event.target.name]: event.target.files![0]
+        [event.target.name]: event.target.files![0],
       });
     } else {
       setFormData({
         ...formData,
-        [event.target.name]: event.target.value
+        [event.target.name]: event.target.value,
       });
     }
   };
@@ -31,17 +37,31 @@ const Home: React.FC = () => {
   };
 
   const carMakes = ['Subaru', 'Fiat', 'Suzuki'];
-  const carModels = ['Impreza', '500', 'Suzuki'];
-  const carBadges = ['Subaru', 'Fiat', 'Suzuki'];
+  const carModels = {
+    Subaru: ['Impreza', 'Outback', 'Forester', 'Crosstrek', 'Legacy'],
+    Fiat: ['500', 'Panda', 'Tipo', 'Bravo', 'Spider'],
+    Suzuki: ['Swift', 'Vitara', 'Ignis', 'S-Cross', 'Baleno'],
+  };
+  const carBadges = {
+    Subaru: ['Badge1', 'Badge2', 'Badge3', 'Badge4', 'Badge5'],
+    Fiat: ['BadgeA', 'BadgeB', 'BadgeC', 'BadgeD', 'BadgeE'],
+    Suzuki: ['BadgeX', 'BadgeY', 'BadgeZ', 'BadgeW', 'BadgeV'],
+  };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl font-bold mb-2 text-center">Select your Broom broom</h1>
       <div className="mb-4 flex space-x-4">
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md" onClick={() => setFormData({ ...formData, make: 'Subaru', model: 'Impreza' })}>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md"
+          onClick={() => setFormData({ ...formData, make: 'Subaru', model: 'Impreza', badge: 'Badge1' })}
+        >
           Subaru Impreza
         </button>
-        <button className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md" onClick={() => setFormData({ ...formData, make: 'Fiat', model: '500' })}>
+        <button
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md"
+          onClick={() => setFormData({ ...formData, make: 'Fiat', model: '500', badge: 'BadgeA' })}
+        >
           Fiat 500
         </button>
       </div>
@@ -55,34 +75,36 @@ const Home: React.FC = () => {
             onChange={handleChange}
           />
         </div>
-        <div className="w-full md:w-auto md:flex-grow md:mr-4 mb-4">
-          <Dropdown
-            label="Model"
-            name="model"
-            options={carModels}
-            value={formData.model}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="w-full md:w-auto md:flex-grow mb-4">
-          <Dropdown
-            label="Badge"
-            name="badge"
-            options={carBadges}
-            value={formData.badge}
-            onChange={handleChange}
-          />
-        </div>
+        {formData.make && (
+          <div className="w-full md:w-auto md:flex-grow md:mr-4 mb-4">
+            <Dropdown
+              label="Model"
+              name="model"
+              options={carModels[formData.make]}
+              value={formData.model}
+              onChange={handleChange}
+            />
+          </div>
+        )}
+        {formData.model && (
+          <div className="w-full md:w-auto md:flex-grow mb-4">
+            <Dropdown
+              label="Badge"
+              name="badge"
+              options={carBadges[formData.make]}
+              value={formData.badge}
+              onChange={handleChange}
+            />
+          </div>
+        )}
         <div className="w-full">
           <div className="text-xl font-bold mb-2">Upload logbook</div>
-          <input
-            type="file"
-            name="file"
-            className="mb-4"
-            onChange={handleChange}
-          />
+          <input type="file" name="file" className="mb-4" onChange={handleChange} />
         </div>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded-md"
+        >
           Submit
         </button>
       </form>
@@ -91,4 +113,3 @@ const Home: React.FC = () => {
 };
 
 export default Home;
-
