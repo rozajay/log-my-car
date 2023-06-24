f685fdf07ab94aaf1782a25b62b593705b42cd2f
Roza <junzhujiang@gmail.com>
2023-06-24 22:13:00 +1000

    fix: fix up formatting

--- frontend/app/details.tsx
+++ frontend/app/details.tsx
@@ -2,12 +2,29 @@ import { useLocation } from 'react-router-dom';
 
 const Details = () => {
     const location = useLocation();
+    const data = JSON.parse(location.state);
+    const { carMake, carModel, carBadge, logContent } = data;
+
     return (
-        <div>
-            <h2>Details</h2>
-            <pre>{JSON.stringify(location.state, null, 2)}</pre>
-        </div>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div>
+                <h2>Details</h2>
+                <div>
+                    <strong>Car Make:</strong> {carMake}
+                </div>
+                <div>
+                    <strong>Car Model:</strong> {carModel}
+                </div>
+                <div>
+                    <strong>Car Badge:</strong> {carBadge}
+                </div>
+                <div>
+                    <strong>Log Book:</strong> {logContent}
+                </div>
+
+            </div>
+        </main>
     );
 };
 
-export default Details
+export default Details;
