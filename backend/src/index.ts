a498eadcae8f6af432f4b152888a9e67a46716db
Roza <junzhujiang@gmail.com>
2023-06-24 14:16:54 +1000

    feat: endpoint for car log

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,25 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 const app = express();
 const port = 3003;
 
-// Define a route handler for the root path
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// POST endpoint for car logs
+app.post('/carlogs', (req: Request, res: Response) => {
+  const { carMake, carModel, carBadge, logContent } = req.body;
+
+  // Perform any desired operations with the received data
+  // For this example, we'll simply log the data to the console
+
+  console.log('Car Make:', carMake);
+  console.log('Car Model:', carModel);
+  console.log('Car Badge:', carBadge);
+  console.log('Log Content:', logContent);
+
+  // Send a response indicating successful processing
+  res.status(200).json({ message: 'Car log received successfully' });
 });
 
 // Start the server
